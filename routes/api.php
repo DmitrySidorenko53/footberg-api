8590ad53ab4694211d74b9d82e3555395a5009f6
dmitry <dimasidorenko53@gmail.com>
2024-06-04 17:39:57 +0300

    add version

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,23 @@
 use App\Http\Controllers\Api\V1\SecurityController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(
     [
-        'prefix' => 'security'
+        'prefix' => 'v1'
     ],
     function () {
-        Route::post('/register', [SecurityController::class, 'register']);
-        Route::post('/login', [SecurityController::class, 'login']);
-    }
-);
+
+        Route::group(
+            [
+                'prefix' => 'security'
+            ],
+            function () {
+                Route::post('/register', [SecurityController::class, 'register']);
+                Route::post('/login', [SecurityController::class, 'login']);
+            }
+        );
+
+
+    });
+
