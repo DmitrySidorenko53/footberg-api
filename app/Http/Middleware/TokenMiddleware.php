a2c49cb9faa0edd7ca3854f3f819d14ce1c71df8
dmitry <dimasidorenko53@gmail.com>
2024-06-25 13:05:36 +0300

    refactor: token middleware

---
+++ app/Http/Middleware/TokenMiddleware.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Helpers\StringGenerator;
+use App\Http\Responses\ApiFailResponse;
+use App\Models\SecurityToken;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class TokenMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $authHeader = $request->header('Authorization');
+
+        if (!$authHeader) {
+            return new ApiFailResponse([], 401, 'There is no authorization header');
+        }
+
+        $authType = config('auth.api_auth_type');
+
+        if (!str_starts_with($authHeader, $authType)) {
+            return new ApiFailResponse([], 400, 'Invalid authorization type');
+        }
+
+        $tokenCandidate = substr($authHeader, strlen($authType) + 1);
+
+        $tokenTemplateStart = StringGenerator::getSecurityTokenStart();
+
+        $lengthOfTokenStart = strlen($tokenTemplateStart);
+
+        $tokenCandidateStart = substr($tokenCandidate, 0, $lengthOfTokenStart);
+        $tokenCandidateStart = trim($tokenCandidateStart);
+
+        if ($tokenTemplateStart !== $tokenCandidateStart) {
+            return new ApiFailResponse([], 400, 'Invalid token format');
+        }
+
+        $tokenCandidate = substr($tokenCandidate, $lengthOfTokenStart);
+
+        /** @var SecurityToken $token */
+        $token = SecurityToken::query()->where('token', $tokenCandidate)
+            ->where('is_valid', true)
+            ->where('is_deleted', false)
+            ->with('user')
+            ->first();
+
+        if (!$token) {
+            return new ApiFailResponse([], 400, 'Invalid token');
+        }
+
+        if ($token->valid_until < now()) {
+            return new ApiFailResponse([], 400, 'Token is expired. Need to refresh token');
+        }
+
+        $candidate = $token->user;
+
+        if ($candidate) {
+            Auth::setUser($candidate);
+        }
+
+        return $next($request);
+    }
+}
